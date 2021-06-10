fafdaef487b4a1f43a187ac24498c91818ec28d4
woodensRangerWZJ <13029768866@163.com>
2021-06-10 13:59:49 +0800

    Ranger Init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from "./store";
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
 import Vant from 'vant';
 import 'vant/lib/index.css';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 createApp(App).use(store).use(Antd).use(Vant).mount('#app')
